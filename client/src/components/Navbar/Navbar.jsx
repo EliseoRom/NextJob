d539cec28b0537d5d93fba72851af1bed78654aa
Eliseo Romero <lucasaruga@Macbook-Pro-Eliseo.local>
2023-04-14 13:20:17 -0300

    Fix: add Components and rutes App

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,77 @@
+import './Navbar.css';
+import { useContext } from 'react';
+import { context } from '../../App';
+import { NavLink } from 'react-router-dom';
+import axios from 'axios';
+
+// Modificar
+function Top() {
+	const logout = () => {
+		axios.get(API).then(() => console.log('token eliminado'));
+	};
+
+	const state = useContext(context);
+	return (
+		<div className='top'>
+			<NavLink to='/'>
+				<button className='inclined-button' autoFocus>
+					HOME
+				</button>
+			</NavLink>
+			{state.state ? (
+				<NavLink to='/news'>
+					<button className='inclined-button'>NEXT JOBS</button>
+				</NavLink>
+			) : null}
+			{state.state ? (
+				<button
+					onClick={() => {
+						state.setMenu(!state.menu);
+						state.setOut(!state.out);
+					}}
+					className='inclined-button'
+				>
+					<i className='fas fa-bars'></i>
+				</button>
+			) : null}
+			{state.state && state.favorite.length > 0 ? (
+				<NavLink to='/favorites'>
+					<button className='inclined-button'>
+						<i className='far fa-heart'></i>
+					</button>
+				</NavLink>
+			) : null}
+
+			{state.state ? (
+				<>
+					<button
+						className=' inclined-button'
+						onClick={() => {
+							logout();
+							state.setState(false);
+						}}
+					>
+						<i className='fas fa-sign-out-alt' />
+					</button>
+				</>
+			) : (
+				<>
+					<NavLink to='/login'>
+						<button className='inclined-button'>LOG IN</button>
+					</NavLink>
+					<NavLink to='/register'>
+						<button className='inclined-button'>SING UP</button>
+					</NavLink>
+				</>
+			)}
+			<button
+				onClick={() => window.open('https://walink.co/2e3987')}
+				className='inclined-button'
+			>
+				<i className='fab fa-whatsapp'></i>
+			</button>
+		</div>
+	);
+}
+
+export default Top;
---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+
+
+
+function Home() {
+    return (
+        <div>
+            <p> Hola Home  estoy aca prueba</p>
+        </div>
+    )
+}
+
+export default Home;
